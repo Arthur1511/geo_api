78dd937b02420e236f82572fdfb103728801ce6b
leonelmota <leonelmota5@gmail.com>
2022-04-24 16:17:46 -0300

    voltando

--- backend/__init__.py
+++ backend/__init__.py
@@ -23,7 +23,7 @@ def create_app():
     return app
 
 
-app = create_app()
+# app = create_app()
 
-if __name__ == '__main__':
-        app.run(debug=True, port=5003)
+# if __name__ == '__main__':
+#         app.run(debug=True, port=5003)
